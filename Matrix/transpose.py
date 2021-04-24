8ecc3016eea1c3e06de22e14475c3b28301594d4
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-04-24 01:33:47 -0400

    matrix transpose

--- Matrix/transpose.py
+++ Matrix/transpose.py
@@ -1,29 +1,17 @@
-matrix = [[1, 2, 3], [4, 5, 6]]
-transpose = []
+def matrix_transpose(m):
+    matrix = [[1, 2, 3], [4, 5, 6]]
+    transpose = []
+    for _ in range(len(matrix[0])):
+        li = [0 for _ in range(len(matrix))]
+        transpose.append(li)
+    # print(transpose)
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            transpose[col][row] = matrix[row][col]
+    return transpose
 
 
-for _ in range(len(matrix[0])):
-
-    li = [0 for _ in range(len(matrix))]
-    transpose.append(li)
-# print(transpose)
-
-for row in range(len(matrix)):
-    for col in range(len(matrix[0])):
-        transpose[col][row] = matrix[row][col]
+matrix_m = [[1, 2, 3], [4, 5, 6]]
+transpose = matrix_transpose(matrix_m)
 print(transpose)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
